8500a71aabac3e36bcd94ec3b4b9ce99388dd271
noah james <yanganoah404@gmail.com>
2019-08-05 05:43:27 +0800

    test commit v2

--- app/src/main/java/com/example/moneytracker/MainActivity.java
+++ app/src/main/java/com/example/moneytracker/MainActivity.java
@@ -9,16 +9,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test file 321321
-        //coomittt321321 21321321
+        //test file 32132131232 213 321 31
+        //coomittt321321 21321321321321
         /**
-         * ewqewqewqewqeqw
-         * ewqewq2132132 321321
-         * sample 2
+         * ewqewqewqewqeqw321321
+         * ewqewq2132132 321321321321
+         * sample 2 321
         * this is a testassssssss
-         * Rica
-         * ajeofnopwenfpownfpo
-         * ewqewqeqw
+         * Rica 321321312
+         * ajeofnopwenfpownfpo321 3 12 3
+         * ewqewqeqw  231321312312
         * */
     }
 
